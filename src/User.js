01b534d7ec2550be140928b37939731e9e8e245a
Vinicius Andrade <viniciusandrade515@hotmail.com>
2019-10-19 16:21:31 -0300

    Design for user's informations

--- src/User.js
+++ src/User.js
@@ -1,59 +1,117 @@
 import React, { Component } from 'react'
-import {View, Text, AppRegistry, StyleSheet, Image, Dimensions, Button} from 'react-native'
+import {ScrollView, View, Text, TextInput, StyleSheet, Image, Dimensions, Button} from 'react-native'
 import MainButton from './MainButton'
 
 
 class User extends Component {
     render() {
         return (
-            <View style={styles.father}>
-                <View>
+            <ScrollView style={styles.father}>
+                <View style={styles.header}>
+
                     <Image source={require('./images/avatar.png')} style={styles.avatarImage} />
+
+                    <View style={styles.buttonView}>
+                        <View style={styles.fixToText}>
+                            <MainButton name='Editar'/>
+                        </View>
+                        <View style={styles.fixToText}>
+                            <MainButton name='Excluir'/>
+                        </View>
+                    </View>
                 </View>
 
-                <View style={styles.buttonView}>  
-                    <View style={styles.fixToText}>
-                        <MainButton name='Editar'/>
-                    </View>
-                    <View style={styles.fixToText}>
-                        <MainButton name='Excluir'/>
-                    </View>
-                </View>
+                <View style={styles.main}>
+                    <Text style={{fontSize: 30, alignSelf: 'center'}}> 'Nome Do Usuário' </Text>
 
-                <View style={{margin:50}}>
-                    <Text> Lista usuario</Text>
+                    <Text style={styles.info} > Email </Text>
+                    <Text style={styles.informacao}> </Text>
+
+                    <Text style={styles.info} > Número</Text>
+                    <Text style={styles.informacao}> </Text>
+
+                    <Text style={styles.info} > Sexo </Text>
+                    <Text style={styles.informacao}> </Text>
+
+                    <Text style={styles.info} > Data de nascimento</Text>
+                    <Text style={styles.informacao}> </Text>
+
+                    <View style={styles.fixToText}>
+                        <MainButton name='Agendamentos'/>
+                    </View>
                 </View>
                 
-
-            </View>
+            </ScrollView>
         )
     }
 }
 
 const styles = StyleSheet.create({
     father:{
+        display: 'flex',
         flex: 1,
-        backgroundColor: '#F8EFBA',
+        backgroundColor: '#ffe0b3',
+    },
+
+    info:{
+        fontSize: 15,
+        marginLeft: 10,
+        marginTop: 20,
+    },
+
+    informacao:{
+        marginTop: 20,
+        borderBottomWidth: 0.8,
+        marginBottom: 5,
+        marginLeft: 10,
+        marginRight: 10,
+    },
+
+    titulo:{
+        fontSize: 30,
+        //fontFamily: 'Open Sans Regular',
+        //fontWeight:'800',
+    },
+
+    main:{
+        display: 'flex',        
+        flexDirection: 'column',
+        marginTop: 25,
+        padding: 10,
+        borderWidth: 1,
+        borderRadius: 10,
+        margin: 10,
+    },
+
+
+    input:{
+        borderWidth: 1,
+        borderRadius: 20,
+        marginTop: 20,
+        marginLeft: 20,
+        marginRight: 20,
+
     },
 
     avatarImage: {
       height: 100,
       width: 100,
-      marginTop: 40,
-      marginLeft: 20,
-      position: 'absolute',
+      position: "relative",
+      marginLeft: 25,
       
     },
 
-    fixToText: {
-        margin: 7,
+    header: {
+        display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'flex-end',
+        marginTop: 30,
     },
 
     buttonView: {
-        marginTop: 40,
-        marginRight: 20,
+        width: 70,
+        marginTop: 5,
+        marginLeft: 130,
+        justifyContent: 'space-around',
     },
 });
 

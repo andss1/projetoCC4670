f23dbb7019121a80e96cc4ec02baf10852a0e4ed
mainlee <simao.anderson4@gmail.com>
2019-10-27 15:45:07 -0300

    Edição do template de listagem e criação do componente de informações dos usuários cadastrados

---
+++ src/UserBalloon.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react'
+import { View , StyleSheet, Text, ScrollView} from 'react-native'
+
+class UserBalloon extends Component {
+    render() {
+        return (
+            <View style={styles.ballon}>
+                <View style={styles.iconUser}>
+                </View>
+                <View style={styles.textUser}>
+                    <Text style={{fontSize: 12, marginLeft: 5}}>Nome: {this.props.name}</Text>
+                    <Text style={{fontSize: 12, marginLeft: 5}}>Email: {this.props.email}</Text>
+                    <Text style={{fontSize: 12, marginLeft: 5}}>Telefone: {this.props.tel}</Text>
+                    <Text style={{fontSize: 12, marginLeft: 5}}>Sexo: {this.props.sexo}</Text>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    ballon: {
+        display: "flex", 
+        flex: 1,
+        flexDirection: "row",
+        backgroundColor: '#ffe0a3',
+        borderWidth : 1.5,
+        borderColor: 'black',
+        padding: 5,
+        borderRadius: 20,
+        height: 80,
+        marginBottom: 8,
+        marginLeft: 10,
+        marginRight: 10,
+    },
+
+    iconUser:{
+        display: "flex",
+        flex: 1,
+        borderColor: "red",
+        borderWidth: 1,
+        backgroundColor: "red",
+        width: 50,
+
+    },
+    
+    textUser:{
+        display: "flex",
+        flex: 3,
+        //borderColor: "green", //retirar
+        //borderWidth: 2, //retirar
+        //backgroundColor: "green"
+    },
+});
+
+export default UserBalloon;
--- src/list.js
+++ src/list.js
@@ -1,29 +1,58 @@
 import React, { Component } from 'react'
-import { View , TouchableOpacity, Image, StyleSheet} from 'react-native'
+import { View , StyleSheet, Text, ScrollView} from 'react-native'
 import {createStackNavigator} from 'react-navigation-stack'
-import {Radio, Body, Text} from 'native-base'
-import MainButton from './MainButton'
+import {Radio, Button, Left, Icon} from 'native-base'
+import UserBalloon from './UserBalloon'
 
 class list extends Component {
+    state = {
+        mas: false,
+        fem: false,
+        all: false
+    }
+
+    masPressed(){
+        this.setState({mas:true, fem:false, all: false});
+    }
+
+    femPressed(){
+        this.setState({mas:false, fem:true, all:false});
+    }
+    allPressed(){
+        this.setState({mas:false, fem:false, all:true});
+    }
+
     render() {
         return (
             <View style={{display: "flex", flex: 1, backgroundColor: "#ffe0b3"}}>
-                <View style={styles.header}> 
-                    <View style={{display: "flex", flex :1, borderWidth : 4,
-        borderColor: 'blue',flexDirection: 'row',}}>
-                        <Radio checked={false} color="#660040"/>
-                        <Text>Masculino</Text>
-                        
-                    </View>
-                    <View style={{display: "flex", flex :1, borderWidth : 4,
-        borderColor: 'green',flexDirection: 'row',}}>
-                        <Radio checked={false} color="#660040"/>
-                            <Text>Feminino</Text>
+                <Button transparent>
+                    <Icon name="arrow-back" color="black"/>
+                </Button>
+                <View style={styles.header}>
+                    <Text style={{fontSize: 25}}>Selecione o sexo:</Text>
+                    <View style={styles.selectView}> 
+                        <View style={{display: "flex", flex :1,flexDirection: 'row',}}>
+                            <Radio selected={this.state.mas} color="#660040" selectedColor="#660040"
+                            onPress={() => this.masPressed()}/>
+                            <Text style={{marginLeft: 7}}>Masculino</Text>
+                        </View>
+                        <View style={{display: "flex", flex :1, flexDirection: 'row',}}>
+                            <Radio selected={this.state.fem} color="#660040" selectedColor="#660040"
+                            onPress={() => this.femPressed()}/>
+                                <Text style={{marginLeft: 7}}>Feminino</Text>
+                        </View>
+                        <View style={{display: "flex", flex :1, flexDirection: 'row',}}>
+                            <Radio selected={this.state.all} color="#660040" selectedColor="#660040"
+                            onPress={() => this.allPressed()}/>
+                                <Text style={{marginLeft: 7}}>Ambos</Text>
+                        </View>
                     </View>
                 </View>
-                <View style={styles.body}>
-
-                </View>
+                <ScrollView style={styles.body}>
+                    <UserBalloon 
+                    name='Anderson' tel='12312312' sexo='M' email='anderson@reactnative'/>
+                    <UserBalloon />
+                </ScrollView>
             </View>
         )
     }
@@ -47,27 +76,34 @@ const styles = StyleSheet.create({
         marginBottom: 100,
     },
     header: {
+        display: "flex",
+        justifyContent: 'space-around',
+        marginTop: 1,
+        marginLeft: 20,
+        marginRight: 20,
+        //borderWidth: 4,
+        //borderColor: 'purple',
+    },
+    selectView: {
         display: 'flex',
         flexDirection: 'row',
         justifyContent: 'space-around',
-        marginTop: 30,
-        marginLeft: 20,
-        marginRight: 20,
-        borderWidth : 4,
-        borderColor: 'red',
+        margin: 5
+        //borderWidth : 4,
+        //borderColor: 'red',
     },
     body: {
         display: 'flex',
         flex: 4,
-        borderWidth : 1.5, // retirar
-        //borderColor: 'black', //retirar
+        borderWidth : 2, // retirar
+        borderColor: 'black', //retirar
         padding: 5,
-        borderRadius: 20,
-        marginTop: 30,
-        marginLeft: 20,
-        marginRight: 20,
+        borderRadius: 10,
+        marginTop: 10,
+        marginLeft: 10,
+        marginRight: 10,
         marginBottom: 5,
-        backgroundColor: '#ffe0a3'
+        backgroundColor: "#550030"
     },
 });
 
